513f3c9f7c7b573a98f8c680b2f32eee785539cf
fecca <niklas.cronfalk@gmail.com>
2018-07-06 00:38:11 +0200

    Unit selection.

---
+++ Assets/UnitCollider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitCollider : MonoBehaviour
+{
+	private List<GameObject> m_colliders = new List<GameObject>();
+
+	private void OnTriggerEnter(Collider other)
+	{
+		m_colliders.Add(other.gameObject);
+		other.GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		m_colliders.Remove(other.gameObject);
+		other.GetComponent<Renderer>().material.color = Color.white;
+	}
+
+	public List<GameObject> GetUnits()
+	{
+		return m_colliders;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(transform.position, transform.localScale);
+	}
+}
---
+++ Assets/UnitSelection.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class UnitSelection : MonoBehaviour
+{
+	[SerializeField]
+	private LayerMask m_layerMask;
+	[SerializeField]
+	private LineRenderer m_lineRenderer;
+	[SerializeField]
+	private UnitCollider m_colliderPrefab;
+
+	private Vector3 m_startPosition;
+	private bool m_mouseDown;
+	private Vector3 point0;
+	private Vector3 point1;
+	private Vector3 point2;
+	private Vector3 point3;
+	private UnitCollider m_collider;
+
+	private void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 500f, m_layerMask))
+			{
+				if (!m_mouseDown)
+				{
+					m_startPosition = hit.point;
+					m_mouseDown = true;
+					m_collider = Instantiate(m_colliderPrefab) as UnitCollider;
+				}
+
+				point0 = m_startPosition + Vector3.up;
+				point1 = new Vector3(m_startPosition.x, m_startPosition.y, hit.point.z) + Vector3.up;
+				point2 = new Vector3(hit.point.x, m_startPosition.y, hit.point.z) + Vector3.up;
+				point3 = new Vector3(hit.point.x, m_startPosition.y, m_startPosition.z) + Vector3.up;
+
+				m_lineRenderer.positionCount = 4;
+				m_lineRenderer.SetPositions(new Vector3[] { point0, point1, point2, point3 });
+
+				m_collider.transform.position = Vector3.Lerp(point0, point2, 0.5f);
+				m_collider.transform.localScale = new Vector3((point1 - point2).magnitude, 1f, (point0 - point1).magnitude);
+			}
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (m_mouseDown)
+			{
+				var colliders = m_collider.GetUnits();
+				Debug.Log($"Selected {colliders.Count} units ");
+
+				m_mouseDown = false;
+				m_lineRenderer.positionCount = 0;
+
+				Destroy(m_collider.gameObject);
+			}
+		}
+	}
+}
